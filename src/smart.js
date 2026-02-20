0739ed2dab79f6c7324475aac49611cf6c0524dc
Mustafa Yiğit Güzel <122071982+Mustafaguzel2@users.noreply.github.com>
2026-02-20 11:24:42 +0300

    smart function updated with four main math operations (#3)
    * smart function updated with four main math operations
    
    * emojis are deleted and used different integers for different operatios

--- src/smart.js
+++ src/smart.js
@@ -1,6 +1,22 @@
+function sum(a, b) {
+  return console.log(`The sum of ${a} and ${b} is ${a + b}`);
+}
+
+function subtract(a, b) {
+  return console.log(`The difference of ${a} and ${b} is ${a - b}`);
+}
+
+function multiply(a, b) {
+  return console.log(`The product of ${a} and ${b} is ${a * b}`);
+}
+
+function divide(a, b) {
+  return console.log(`The quotient of ${a} and ${b} is ${a / b}`);
+}
+
 export function smart() {
-  const a = 1;
-  const b = 2;
-  const c = a + b;
-  console.log(`The sum of ${a} and ${b} is ${c} 😊`);
+  sum(1, 2);
+  subtract(3, 4);
+  multiply(5, 6);
+  divide(7, 8);
 }
