ebac99e20f4e0a70f41914cbd608745a573c5fc0
Mustafa Yiğit Güzel <mustafaguzel879@gmail.com>
2026-02-20 11:23:35 +0300

    emojis are deleted and used different integers for different operatios

--- src/smart.js
+++ src/smart.js
@@ -1,22 +1,22 @@
 function sum(a, b) {
-  return console.log(`The sum of ${a} and ${b} is ${a + b} 😊`);
+  return console.log(`The sum of ${a} and ${b} is ${a + b}`);
 }
 
 function subtract(a, b) {
-  return console.log(`The difference of ${a} and ${b} is ${a - b} 😊`);
+  return console.log(`The difference of ${a} and ${b} is ${a - b}`);
 }
 
 function multiply(a, b) {
-  return console.log(`The product of ${a} and ${b} is ${a * b} 😊`);
+  return console.log(`The product of ${a} and ${b} is ${a * b}`);
 }
 
 function divide(a, b) {
-  return console.log(`The quotient of ${a} and ${b} is ${a / b} 😊`);
+  return console.log(`The quotient of ${a} and ${b} is ${a / b}`);
 }
 
 export function smart() {
   sum(1, 2);
-  subtract(1, 2);
-  multiply(1, 2);
-  divide(1, 2);
+  subtract(3, 4);
+  multiply(5, 6);
+  divide(7, 8);
 }
