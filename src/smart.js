e9e23a379651204dac5897420c24949c7acf6e86
Mustafa Yiğit Güzel <mustafaguzel879@gmail.com>
2026-02-20 11:21:18 +0300

    smart function updated with four main math operations

--- src/smart.js
+++ src/smart.js
@@ -1,6 +1,22 @@
+function sum(a, b) {
+  return console.log(`The sum of ${a} and ${b} is ${a + b} 😊`);
+}
+
+function subtract(a, b) {
+  return console.log(`The difference of ${a} and ${b} is ${a - b} 😊`);
+}
+
+function multiply(a, b) {
+  return console.log(`The product of ${a} and ${b} is ${a * b} 😊`);
+}
+
+function divide(a, b) {
+  return console.log(`The quotient of ${a} and ${b} is ${a / b} 😊`);
+}
+
 export function smart() {
-  const a = 1;
-  const b = 2;
-  const c = a + b;
-  console.log(`The sum of ${a} and ${b} is ${c} 😊`);
+  sum(1, 2);
+  subtract(1, 2);
+  multiply(1, 2);
+  divide(1, 2);
 }
